563e53820c9bf351db2bcd7060eb4e026c8d7561
Valera <2.px.chanel@gmail.com>
2023-08-21 19:11:10 +0300

    lesson 23
    functions

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ showMyDB(personalMovieDB.privat);
 
 function writeYourGenres() {
     for(let i = 1; i <= 3; i++){
-        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
     }
 }
 
