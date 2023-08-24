a509a9a696fd03aee2dfe8c1630ade62a879ca8c
Valera <2.px.chanel@gmail.com>
2023-08-24 14:23:03 +0300

    added .trim()
    minus spaces

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ const personalMovieDB = {
 
 function remerberMyFilms() {
     for (let i = 0; i < 2; i++) {
-        const   a = prompt('Один из последних просмотренных фильмов?',''),
-                b = prompt('На сколько оцените его?', '');
+        const   a = prompt('Один из последних просмотренных фильмов?','').trim(),
+                b = +prompt('На сколько оцените его?', '');
     
         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
             personalMovieDB.movies[a] = b;
