172b25e786b91420dbd6240228a633adeaa91be9
Valera <2.px.chanel@gmail.com>
2023-09-11 15:00:10 +0300

    lesson 31
    project_movie

--- script.js
+++ script.js
@@ -1,73 +1,79 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
-    genres: [],
-    privat: false
-};
+    genres: {},
+    privat: false,
 
-
-function remerberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const   a = prompt('Один из последних просмотренных фильмов?','').trim(),
-                b = +prompt('На сколько оцените его?', '');
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
     
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('all done');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+
+    remerberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const   a = prompt('Один из последних просмотренных фильмов?','').trim(),
+                    b = +prompt('На сколько оцените его?', '');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('all done');
+            } else {
+                console.log('error');
+                i--;
+            } 
+    }
+    },
+
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log ('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Вы классический зритель');
+        } else if (personalMovieDB.count > 30) {
+            console.log ('Вы киноман');
         } else {
-            console.log('error');
-            i--;
-        } 
-}
-}
+            console.log('Произошла ошибка');
+        }
+    
+    },
 
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
 
-remerberMyFilms();
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
 
+    writeYourGenres: function() {
+        for(let i = 1; i < 2; i++){
+            let genres = prompt(`Введите ваши любимие жанри через запятую`).toLowerCase();
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log ('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('Вы классический зритель');
-    } else if (personalMovieDB.count > 30) {
-        console.log ('Вы киноман');
-    } else {
-        console.log('Произошла ошибка');
+            if (genres === '' || genres == null) {
+                console.log('Вы ввели некоректные данные или ввели их вовсе')
+                i--;
+            } else {
+                personalMovieDB.genres = genres.split(', ');
+                personalMovieDB.genres.sort();
+            } 
+        }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
     }
-
-}
-
-detectPersonalLevel();
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for(let i = 1; i <= 3; i++){
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-
-writeYourGenres();
+};
 
